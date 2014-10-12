42a2c676259a8727b82635d336bed0696d21044f
ausmeyer <austin.g.meyer@gmail.com>
2014-10-11 22:53:52 -0500

    Updating figure.

--- analysis/plot_epitopes.R
+++ analysis/plot_epitopes.R
@@ -1,5 +1,8 @@
 rm(list = ls())
+library(ggplot2)
+library(grid)
 
+setwd('~/Google Drive/Data/influenza_HA_evolution/')
 ep.A<-c(122, 124, 126, 130, 131, 132, 133, 135, 137, 138, 140, 142, 143, 144, 145, 146, 150, 152, 168)
 ep.B<-c(128, 129, 155, 156, 157, 158, 159, 160, 163, 164, 165, 186, 187, 188, 189, 190, 192, 193, 194, 196, 197, 198)
 ep.C<-c(44, 45, 46, 47, 48, 50, 51, 53, 54, 273, 275, 276, 278, 279, 280, 294, 297, 299, 300, 304, 305, 307, 308, 309, 310, 311, 312)
@@ -9,10 +12,10 @@ ep.total<-c(ep.A, ep.B, ep.C, ep.D, ep.E)
 ep.exp<-c(18,  20,  34,  37,  38,  54,  66,  67,  98, 105, 106, 107, 108, 110, 115, 117, 119, 120, 121, 122, 124, 126, 127, 128, 129, 130, 131, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 155, 156, 157, 158, 159, 160, 173, 174, 175, 176, 177, 178, 179, 180, 181, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 222, 225, 226, 259, 260, 262, 276, 278, 289, 291, 318, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 347, 348, 349, 350, 363, 364, 366, 367, 368, 370, 371, 374, 375, 377, 378, 379, 381, 382, 385, 387, 388, 389, 482)
 deem<-c(122, 124, 126, 131, 133, 137, 140, 142, 144, 145, 128, 155, 156, 157, 158, 159, 189, 192, 193, 197, 45, 50, 273, 275, 278, 312, 121, 172, 173, 201, 207, 219, 226, 227, 229, 246, 57, 62, 75, 78, 83, 92, 260, 262, 3, 5, 25, 33, 49, 106, 202, 222, 225, 271)
 
-t.linear.eps <- (read.table('../epitope_data/t_linear.counts', head=T))[17:566, ]
-b.linear.eps <- (read.table('../epitope_data/b_linear.counts', head=T))[17:566, ]
-nonlinear.eps <- read.table('../epitope_data/nonlinear_eps.counts', head=T)
-map <- t(read.table('../sequence_data/not_structure/combined/sequences/map.txt', sep=','))
+t.linear.eps <- (read.table('epitope_data/t_linear.counts', head=T))[17:566, ]
+b.linear.eps <- (read.table('epitope_data/b_linear.counts', head=T))[17:566, ]
+nonlinear.eps <- read.table('epitope_data/nonlinear_eps.counts', head=T)
+map <- t(read.table('sequence_data/not_structure/combined/sequences/map.txt', sep=','))
 
 #The epitopes already have the first 16 amino acids cut off for numbering
 #linear.eps.short <- linear.eps$ep_counts[!is.na(as.vector(map[17:566,1]))]
@@ -42,11 +45,12 @@ mycols <- dput(ggplot2like(n = 5, h.start = 0, l = 65)$superpose.line$col)
 
 cbbPalette <- c('A' = mycols[1], 'B' = mycols[2], 'D' = mycols[3], 'C' = mycols[4], 'E' = mycols[5], 'None' = "#000000")
 
-p <- ggplot(aes(x=index, y=b.eps, colour=id, fill=id), data=df) + geom_bar(stat='identity', width=1) + geom_point(size=3) + 
+p <- ggplot(aes(x=index, y=b.eps, colour=id, fill=id), data=df) + geom_bar(stat='identity', width=1) + 
   scale_colour_manual(values=cbbPalette) + 
   scale_fill_manual(values=cbbPalette)
+p <- p + geom_tile(aes(x=index, y=rep(-0.1, length(index)), fill=id), height=0.1)
 p <- p + scale_x_continuous(breaks=seq(0, 550, 100), limits=c(0, 550))
-p <- p + scale_y_continuous(breaks=seq(0, 5, 1), limits=c(0, 5))
+p <- p + scale_y_continuous(breaks=seq(0, 5, 1), limits=c(-0.2, 5))
 p <- p + ylab('Count in B cell Epitopes')
 p <- p + xlab('Site in Mature Hemagglutinin')
 p <- p + theme_bw()
@@ -95,11 +99,12 @@ mycols <- dput(ggplot2like(n = 5, h.start = 0, l = 65)$superpose.line$col)
 
 cbbPalette <- c('A' = mycols[1], 'B' = mycols[2], 'D' = mycols[3], 'C' = mycols[4], 'E' = mycols[5], 'None' = "#000000")
 
-p <- ggplot(aes(x=index, y=t.eps, colour=id, fill=id), data=df) + geom_bar(stat='identity', width=1) + geom_point(size=3) + 
+p <- ggplot(aes(x=index, y=t.eps, colour=id, fill=id), data=df) + geom_bar(stat='identity', width=1) + 
   scale_colour_manual(values=cbbPalette) + 
   scale_fill_manual(values=cbbPalette)
+p <- p + geom_tile(aes(x=index, y=rep(-0.5, length(index)), fill=id), height=0.5)
 p <- p + scale_x_continuous(breaks=seq(0, 550, 100), limits=c(0, 550))
-#p <- p + scale_y_continuous(breaks=seq(0, 5, 1), limits=c(0, 5))
+p <- p + scale_y_continuous(breaks=seq(0, 25, 5), limits=c(-1, 25))
 p <- p + ylab('Count in T cell Epitopes')
 p <- p + xlab('Site in Mature Hemagglutinin')
 p <- p + theme_bw()
