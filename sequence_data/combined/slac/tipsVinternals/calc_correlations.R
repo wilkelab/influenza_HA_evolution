fbbb34cabd7ea042ec33cb69a3aa855b922a8dc6
ausmeyer <austin.g.meyer@gmail.com>
2015-03-23 18:06:05 -0500

    Added correlation calculator.

---
+++ sequence_data/combined/slac/tipsVinternals/calc_correlations.R
@@ -0,0 +1,25 @@
+rm(list = ls())
+
+distances <- read.table('distances.dat', sep=',')
+
+rates <- read.table('sites.dat', sep='\t', head=T)
+map <- read.table('~/Google Drive/Data/influenza_HA_evolution/data_table/numbering_table_unix.csv', head=T, sep=',')
+map <- map$pdb.4fnk
+map[25] <- NA
+map[26] <- NA
+dN.dS <- (rates$dN/rates$dS)[rates$dS > 0]
+distances <- distances[-1,]
+distances <- distances[-1,]
+distances <- distances[,-1]
+distances <- distances[,-1]
+distances <- distances[rates$dS > 0, ]
+distances <- distances[,rates$dS > 0]
+
+correlations <- as.vector(sapply(distances, function(x) cor(1/x[x!=0], dN.dS[x!=0])))
+p.values <- as.vector(sapply(distances, function(x) cor.test(1/x[x!=0], dN.dS[x!=0])$p.value))
+write.table(data.frame(correlations), file= 'fulltree.correlations', row.names=F, col.names=F)
+print(min(correlations))
+print(max(correlations))
+
+print(which(min(correlations) == correlations))
+print(which(max(correlations) == correlations))
