2e9612675454b43e2d584f49fac0bb35882df7b2
ausmeyer <austin.g.meyer@gmail.com>
2015-03-25 16:16:43 -0500

    Reorganize.

--- sequence_data/combined/slac/alltree/calc_correlations.R
+++ sequence_data/combined/slac/alltree/calc_correlations.R
@@ -3,8 +3,8 @@ rm(list = ls())
 distances <- read.table('distances.dat', sep=',')
 
 rates <- read.table('sites.dat', sep='\t', head=T)
-map <- read.table('~/Google Drive/Data/influenza_HA_evolution/data_table/numbering_table_unix.csv', head=T, sep=',')
-map <- map$pdb.4fnk
+df <- read.table('~/Google Drive/Data/influenza_HA_evolution/data_table/numbering_table_unix.csv', head=T, sep=',')
+map <- df$pdb.4fnk
 map[25] <- NA
 map[26] <- NA
 dN.dS <- rates$dN
@@ -18,9 +18,10 @@ distances <- distances[,-1]
 correlations <- as.vector(sapply(distances, function(x) cor(1/x[x!=0], dN.dS[x!=0])))
 p.values <- as.vector(sapply(distances, function(x) cor.test(1/x[x!=0], dN.dS[x!=0])$p.value))
 write.table(data.frame(correlations), file= 'internals.correlations', row.names=F, col.names=F)
+write.table(data.frame(distances[, which(max(dN.dS) == dN.dS)]), file='best_distances.dat', row.names=F, col.names=F)
 print(min(correlations))
 print(max(correlations))
-print(which(max(dN.dS) == dN.dS))
+print(df$Protein[which(max(dN.dS) == dN.dS)])
 
 print(which(min(correlations) == correlations))
 print(order(correlations))
