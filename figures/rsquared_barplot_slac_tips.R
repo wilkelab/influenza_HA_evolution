54267e4342da74c4e2fd14775662883895c199c5
ausmeyer <austin.g.meyer@gmail.com>
2015-03-26 16:00:40 -0500

    Adding all of the new figures.

--- figures/rsquared_barplot_slac_tips.R
+++ figures/rsquared_barplot_slac_tips.R
@@ -8,37 +8,37 @@ cols = rev(gg_color_hue(3))
 
 d <- read.table('~/Google Drive/Data/influenza_HA_evolution/data_table/numbering_table_unix.csv', sep=',', head=T, stringsAsFactors = F)
 d <- d[!is.na(d$pdb.4fnk), ]
-r.value.1 <- summary(lm(FEL.dN.dS ~ RSA.Multimer, data=d))$r.squared
+r.value.1 <- summary(lm(SLAC.dN.tips ~ RSA.Multimer, data=d))$r.squared
 r.1 <- "RSA"
 
-r.value.2 <- summary(lm(FEL.dN.dS ~ I(1/distance.to.224.all), data=d))$r.squared
+r.value.2 <- summary(lm(SLAC.dN.tips ~ I(1/distance.to.224.all), data=d))$r.squared
 r.2 <- "1 / Distance"
 
-r.value.3 <- summary(lm(FEL.dN.dS ~ Bush.99, data=d))$r.squared
+r.value.3 <- summary(lm(SLAC.dN.tips ~ Bush.99, data=d))$r.squared
 r.3 <- "Bush '99"
 
-r.value.4 <- summary(lm(FEL.dN.dS ~ Meyer.14, data=d))$r.squared
+r.value.4 <- summary(lm(SLAC.dN.tips ~ Meyer.14, data=d))$r.squared
 r.4 <- "Epitopes"
 
-r.value.5 <- summary(lm(FEL.dN.dS ~ I(1 / distance.to.224.all) + Bush.99, data=d))$r.squared
+r.value.5 <- summary(lm(SLAC.dN.tips ~ I(1 / distance.to.224.all) + Bush.99, data=d))$r.squared
 r.5 <- "1 / Distance + Bush '99"
 
-r.value.6 <- summary(lm(FEL.dN.dS ~ I(1 / distance.to.224.all) + Meyer.14 , data=d))$r.squared
+r.value.6 <- summary(lm(SLAC.dN.tips ~ I(1 / distance.to.224.all) + Meyer.14 , data=d))$r.squared
 r.6 <- "1 / Distance + Epitopes"
 
-r.value.7 <- summary(lm(FEL.dN.dS ~ RSA.Multimer + Bush.99, data=d))$r.squared
+r.value.7 <- summary(lm(SLAC.dN.tips ~ RSA.Multimer + Bush.99, data=d))$r.squared
 r.7 <- "RSA + Bush '99"
 
-r.value.8 <- summary(lm(FEL.dN.dS ~ RSA.Multimer + Meyer.14 , data=d))$r.squared
+r.value.8 <- summary(lm(SLAC.dN.tips ~ RSA.Multimer + Meyer.14 , data=d))$r.squared
 r.8 <- "RSA + Epitopes"
 
-r.value.9 <- summary(lm(FEL.dN.dS ~ RSA.Multimer + I(1 / distance.to.224.all), data=d))$r.squared
+r.value.9 <- summary(lm(SLAC.dN.tips ~ RSA.Multimer + I(1 / distance.to.224.all), data=d))$r.squared
 r.9 <- "RSA + 1 / Distance"
 
-r.value.10 <- summary(lm(FEL.dN.dS ~ RSA.Multimer + I(1 / distance.to.224.all) + Bush.99, data=d))$r.squared
+r.value.10 <- summary(lm(SLAC.dN.tips ~ RSA.Multimer + I(1 / distance.to.224.all) + Bush.99, data=d))$r.squared
 r.10 <- "RSA + 1 / Distance + Bush '99"
 
-r.value.11 <- summary(lm(FEL.dN.dS ~ RSA.Multimer + I(1 / distance.to.224.all) + Meyer.14, data=d))$r.squared
+r.value.11 <- summary(lm(SLAC.dN.tips ~ RSA.Multimer + I(1 / distance.to.224.all) + Meyer.14, data=d))$r.squared
 r.11 <- "RSA + 1 / Distance + Epitopes"
 
 rs <- c(r.value.1, r.value.4, r.value.2, r.value.3, r.value.8, r.value.7, r.value.6, r.value.5, r.value.9, r.value.11, r.value.10)
@@ -53,4 +53,4 @@ p <- ggplot(aes(x = names, y = r.square), data = df) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
   scale_y_continuous(limits = c(0, 0.4))
 
-#ggsave("~/Google Drive/Data/influenza_HA_evolution/figures/r_squared.pdf", p, width=7.5, height=7.5)
+ggsave("~/Google Drive/Data/influenza_HA_evolution/figures/r_squared_slac_tips.pdf", p, width=7.5, height=7.5)
